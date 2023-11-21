c346e71144d5a1a9bfc378e3d19b005503dc2b6f
unknown <drunk3n.p1nky@gmail.com>
2023-11-21 19:44:09 +0500

    Task #80

---
+++ Assets/Scripts/Bootstrap/BootstrapFlow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.SceneManagement;
+using Utility.Services;
+using VContainer.Unity;
+using Utility;
+
+namespace Bootstrap
+{
+    public class BootstrapFlow : IStartable
+    {
+        private readonly LoadingService _loadingService;
+
+        public BootstrapFlow(LoadingService loadingService)
+        {
+            _loadingService = loadingService;
+        }
+
+        public async void Start()
+        {
+            await _loadingService.StartLoading(new ApplicationConfiguration());
+            SceneManager.LoadScene(Constants.Scenes.Loading);
+        }
+    }
+}
---
+++ Assets/Scripts/Bootstrap/BootstrapScope.cs
@@ -0,0 +1,23 @@
+using Utility.Services;
+using VContainer;
+using VContainer.Unity;
+
+namespace Bootstrap
+{
+    public class BootstrapScope : LifetimeScope
+    {
+        protected override void Awake()
+        {
+            IsRoot = true;
+            DontDestroyOnLoad(this);
+            base.Awake();
+        }
+
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<LoadingService>(Lifetime.Scoped);
+
+            builder.RegisterEntryPoint<BootstrapFlow>();
+        }
+    }
+}
