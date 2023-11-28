6760c841e95289d521cb4190c758c31aee76064e
unknown <drunk3n.p1nky@gmail.com>
2023-11-28 15:46:16 +0500

    Button, Transitions

---
+++ Assets/Scripts/Utility/UI/ScaleTransition.cs
@@ -0,0 +1,20 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Utility.UI
+{
+    public struct ScaleTransition : ITransition<Transform>
+    {
+        [SerializeField]
+        private float _targetValue;
+        [SerializeField]
+        private float _duration;
+        [SerializeField]
+        private Ease _easeType;
+
+        public Tween Play(Transform transform)
+        {
+            return transform.DOScale(_targetValue, _duration).SetEase(_easeType).SetUpdate(true);
+        }
+    }
+}
